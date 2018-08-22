e0570c1b4f092055e348c42c68e87e40b3f1e4a9
Joana Durvalo <j.durvalo@gmail.com>
2018-08-22 10:38:05 -0300

    create a todo list using vue and bootstrap

---
+++ todo-item.vue
@@ -0,0 +1,26 @@
+
+<template>
+    <div>
+        <input type="radio" v-on:click="deletar" />  
+             {{texto}}  
+    </div>
+</template>
+
+<script>
+
+export default {
+    props:[
+        "texto",
+        "num"
+    ],
+    methods: {
+        deletar(){
+            this.$emit("deletar");
+        }
+    },
+}
+</script>
+
+<style>
+
+</style>
